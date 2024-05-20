4e270e7668f5409a50dfcb469c1c42bac5e6560c
Clayton Horning <clayton.horning@colorado.edu>
2024-05-20 14:52:03 -0600

    Added API to .env

--- src/app/roaringforkvalley/page.tsx
+++ src/app/roaringforkvalley/page.tsx
@@ -5,13 +5,13 @@ import { redirect } from "next/navigation";
 async function getData() {
   const options = {
     method: "GET",
-    headers: {
-      Authorization:
-        "Bearer 14e911851b8b87ca2d3d2ba4cbd2aab8bce5fad9e5b263ea56326a7413eaccc5adb23e51dceb2dea273742eebc2a342e30fda1f8619a836d4d78f42874ae26c15a9eb060e6c888fe0040a78ca490741d8a54be631c798f108886d09cc0d365d0c74d9cdf700014e3073f2459db51c7adbe3cb2d385de9c6c56f1404968e17048",
-    },
+    // headers: {
+    //   Authorization:
+    //     "Bearer 14e911851b8b87ca2d3d2ba4cbd2aab8bce5fad9e5b263ea56326a7413eaccc5adb23e51dceb2dea273742eebc2a342e30fda1f8619a836d4d78f42874ae26c15a9eb060e6c888fe0040a78ca490741d8a54be631c798f108886d09cc0d365d0c74d9cdf700014e3073f2459db51c7adbe3cb2d385de9c6c56f1404968e17048",
+    // },
   };
   const res = await fetch(
-    `http://localhost:3001/categories`,
+    `${process.env.TRUSTEDVINE_API_URL}/categories`,
     options
   );
 
