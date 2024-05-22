a0cda23a8c38afcc4c3801d13cdcff36901f2648
Clayton Horning <clayton.horning@colorado.edu>
2024-05-22 12:43:30 -0600

    Autocomplete and no cache

--- src/app/roaringforkvalley/page.tsx
+++ src/app/roaringforkvalley/page.tsx
@@ -1,14 +1,12 @@
 import { IconAdjustmentsHorizontal } from "@tabler/icons-react";
 import Service from "@/components/service";
 import { redirect } from "next/navigation";
+import AutocompleteSearch from "@/components/ui/AutocompleteSearch";
 
 async function getData() {
-  const options = {
+  const options: RequestInit = {
     method: "GET",
-    // headers: {
-    //   Authorization:
-    //     "Bearer 14e911851b8b87ca2d3d2ba4cbd2aab8bce5fad9e5b263ea56326a7413eaccc5adb23e51dceb2dea273742eebc2a342e30fda1f8619a836d4d78f42874ae26c15a9eb060e6c888fe0040a78ca490741d8a54be631c798f108886d09cc0d365d0c74d9cdf700014e3073f2459db51c7adbe3cb2d385de9c6c56f1404968e17048",
-    // },
+    cache: "no-store",
   };
   const res = await fetch(
     `${process.env.TRUSTEDVINE_API_URL}/categories`,
@@ -38,49 +36,74 @@ export default async function FindLocalContractors() {
 
   const data = await getData();
 
+  const items = data.map((item: any, index: any) => ({
+    id: index,
+    name: item.name,
+  }));
+
+  const handleOnSelect = async (item: any) => {
+    "use server";
+    redirect(
+      `/roaringforkvalley/search?category=${item.name}`
+    );
+  };
+
   return (
     <div className="flex flex-col">
       <h1 className="text-black text-2xl font-medium">
         Search Roaring Fork Valley Businesses
       </h1>
-      <form
-        className="flex flex-row space-x-2 mt-4"
-        action={search}
-      >
-        <input
-          type="search"
-          name="search"
-          className="bg-gray-200 w-5/12 h-12 rounded-md placeholder:text-slate-600 px-4 text-black"
-          placeholder="Search for a service "
-          aria-label="Search"
-        />
-        <button className="flex flex-row items-center bg-gray-200 text-slate-600 px-4 rounded-md">
-          <IconAdjustmentsHorizontal />
-          Filter
-        </button>
-        <button
-          type="submit"
-          className="flex flex-row items-center bg-[#4F772D] px-4 rounded-md text-white"
+
+      <div className="-z-1">
+        <form
+          className="flex flex-row mt-4"
+          action={search}
         >
-          Search
-        </button>
-      </form>
-      <div className="flex flex-col">
-        <h1 className="text-black text-2xl font-medium mt-8 mb-4">
-          All Service Categories
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {data?.map((category: any) => {
-            return (
-              <Service
-                name={category.name}
-                num={category.providerCount}
-                img={category.bannerImage}
-                slug={`/roaringforkvalley/search?category=${category.slug}`}
-                key={category._id}
-              />
-            );
-          })}
+          <div className="z-10">
+            <AutocompleteSearch
+              handleOnSelect={handleOnSelect}
+              items={items}
+            />
+            <input
+              type="hidden"
+              name="search" // Hidden input to capture the search term
+            />
+          </div>
+          {/* <input
+            type="search"
+            name="search"
+            className="bg-gray-200 w-5/12 h-12 rounded-md placeholder:text-slate-600 px-4 text-black mr-2"
+            placeholder="Search for a service "
+            aria-label="Search"
+          /> */}
+          <button className="flex flex-row items-center bg-gray-200 text-slate-600 px-4 rounded-md mr-2">
+            <IconAdjustmentsHorizontal />
+            Filter
+          </button>
+          <button
+            type="submit"
+            className="flex flex-row items-center bg-[#4F772D] px-4 rounded-md text-white"
+          >
+            Search
+          </button>
+        </form>
+        <div className="flex flex-col">
+          <h1 className="text-black text-2xl font-medium mt-8 mb-4">
+            All Service Categories
+          </h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {data?.map((category: any) => {
+              return (
+                <Service
+                  name={category.name}
+                  num={category.providerCount}
+                  img={category.bannerImage}
+                  slug={`/roaringforkvalley/search?category=${category.slug}`}
+                  key={category._id}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/ui/AutocompleteSearch.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
+
+import React from "react";
+
+export default function AutocompleteSearch({
+  items,
+  handleOnSearch,
+  handleOnSelect,
+}: any) {
+  const handleOnHover = (result: any) => {
+    // the item hovered
+    console.log(result);
+  };
+
+  const handleOnFocus = () => {
+    console.log("Focused");
+  };
+
+  const formatResult = (item: any) => {
+    return (
+      <>
+        <span
+          style={{ display: "block", textAlign: "left" }}
+        >
+          {item.name}
+        </span>
+      </>
+    );
+  };
+  return (
+    <div id="name" style={{ width: 400 }} className="mr-2">
+      <ReactSearchAutocomplete
+        items={items}
+        onSearch={handleOnSearch}
+        onHover={handleOnHover}
+        onFocus={handleOnFocus}
+        autoFocus
+        formatResult={formatResult}
+        placeholder="Search for a service"
+        styling={{
+          backgroundColor: "white",
+          borderRadius: "5px",
+        }}
+      />
+    </div>
+  );
+}
