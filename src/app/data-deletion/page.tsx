a20d0b331e0d61a86b8a2878ba21f8917ab367ed
Clayton Horning <clayton.horning@colorado.edu>
2024-05-22 08:58:47 -0600

    Data deletion and privacy policy pages

---
+++ src/app/data-deletion/page.tsx
@@ -0,0 +1,76 @@
+export default function DataDeletion() {
+  return (
+    <div className="container mx-auto text-black flex flex-col space-y-3 py-16">
+      <h1 className="text-2xl pb-6 font-semibold">
+        User Data Deletion Request
+      </h1>
+      <p>
+        If you wish to delete your account with TrustedVine,
+        we are committed to ensuring the safe and complete
+        deletion of your data from our records, in
+        accordance with applicable privacy laws and
+        regulations.
+      </p>
+
+      <h2 className="text-xl font-medium pt-4">
+        How to Request Data Deletion
+      </h2>
+      <p>
+        To request the deletion of your account and
+        associated data, please contact our support team
+        with the details provided below. We will guide you
+        through the process and ensure that your data is
+        handled in compliance with privacy regulations:
+      </p>
+      <ul>
+        <li>Email: clay@trustedvine.com</li>
+        <li>Phone: (970) 987-4747</li>
+      </ul>
+      <p>
+        Please include your account information and a brief
+        statement about your request to delete your data in
+        your communication.
+      </p>
+
+      <h2>What Happens to Your Data?</h2>
+      <p>
+        Upon receiving your request for account deletion, we
+        will take the following steps to remove your data
+        from our systems:
+      </p>
+      <ul>
+        <li>
+          Your request will be processed within 30 days of
+          receiving it.
+        </li>
+        <li>
+          All personal information associated with your
+          account will be deleted from our active databases.
+        </li>
+        <li>
+          Some anonymized data may be retained for
+          analytical purposes without personal identifiers.
+        </li>
+      </ul>
+
+      <h2 className="text-xl font-medium pt-4">
+        Third-Party Services
+      </h2>
+      <p>
+        If you have used any third-party services in
+        connection with our website, such as social media
+        logins or payment services, you may also need to
+        contact those services directly to request the
+        deletion of your data held by them.
+      </p>
+
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions about the data deletion
+        process, or need further assistance, please don't
+        hesitate to contact us using the details provided
+        above. We are here to help.
+      </p>
+    </div>
+  );
+}
---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,152 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="container mx-auto text-black flex flex-col space-y-3 py-16">
+      <h1 className="text-2xl pb-6 font-semibold">
+        Privacy Policy
+      </h1>
+      <p>Last updated: 5/22/2024</p>
+      <p>
+        Welcome to TrustedVine! We are committed to
+        protecting your privacy and ensuring that your
+        personal information is handled safely and
+        responsibly. This Privacy Policy explains how we
+        collect, use, and share information about you when
+        you use our website, TrustedVine.com, and our
+        associated services (collectively, "Services").
+      </p>
+
+      <h2 className="text-xl font-medium pt-4">
+        Information We Collect
+      </h2>
+      <ol>
+        <li>
+          Personal Information
+          <ul>
+            <li>
+              Account Information:When you create an
+              account, we collect information such as your
+              name, email address, and password.
+            </li>
+            <li>
+              Profile Information:You may also choose to
+              provide additional profile information like a
+              profile picture, bio, or location.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Business Recommendations
+          <p>
+            When you recommend a business, we collect the
+            information you provide about the business,
+            including names, contact details, and your
+            experiences.
+          </p>
+        </li>
+        <li>
+          Usage Data
+          <p>
+            We automatically collect information about how
+            you use our services, including your
+            interactions with our website and the businesses
+            you view.
+          </p>
+        </li>
+        <li>
+          Communication Information
+          <p>
+            When you communicate with us or other users, we
+            collect information about these communications,
+            including the date and time.
+          </p>
+        </li>
+      </ol>
+
+      <h2 className="text-xl font-medium pt-4">
+        How We Use Your Information
+      </h2>
+      <ul>
+        <li>
+          To Provide and Improve Our Services: We use your
+          information to operate, maintain, and improve our
+          services. This includes using your information to
+          personalize your experience.
+        </li>
+        <li>
+          Communication: We may use your information to
+          communicate with you about services, updates, and
+          promotions.
+        </li>
+        <li>
+          Research and Analytics: We analyze how users
+          interact with our site to make improvements and
+          develop new features.
+        </li>
+      </ul>
+
+      <h2 className="text-xl font-medium pt-4">
+        How We Share Your Information
+      </h2>
+      <ul>
+        <li>
+          Business Recommendations: Information about
+          recommended businesses is shared with other users
+          of our platform.
+        </li>
+        <li>
+          Service Providers: We may share your information
+          with service providers who perform services on our
+          behalf, such as hosting and maintenance,
+          analytics, customer service, and marketing.
+        </li>
+        <li>
+          Legal Requirements: We may disclose your
+          information if required to do so by law or in the
+          good faith belief that such action is necessary to
+          comply with legal obligations or protect the
+          safety of our users.
+        </li>
+      </ul>
+
+      <h2>Security</h2>
+      <p>
+        We take reasonable measures to protect your
+        information from loss, theft, misuse, and
+        unauthorized access. However, no internet or email
+        transmission is ever fully secure or error-free.
+        Please keep this in mind when disclosing any
+        personal information to us or to any other party via
+        the Internet.
+      </p>
+
+      <h2>Your Choices</h2>
+      <p>
+        You can access, amend, download, and delete your
+        personal information by logging into your account
+        and navigating to your account settings page. You
+        also have the option to opt-out of receiving
+        marketing communications from us.
+      </p>
+
+      <h2>Changes to This Privacy Policy</h2>
+      <p>
+        We may update this Privacy Policy from time to time
+        to reflect changes to our practices or for other
+        operational, legal, or regulatory reasons. If we
+        make changes, we will notify you by revising the
+        date at the top of the policy and, in some cases, we
+        may provide you with additional notice (such as
+        adding a statement to our homepage or sending you a
+        notification).
+      </p>
+
+      <h2 className="text-xl font-medium pt-4">
+        Contact Us
+      </h2>
+      <p>
+        If you have any questions about this Privacy Policy,
+        please contact us at clay@trustedvine.com.
+      </p>
+    </div>
+  );
+}
