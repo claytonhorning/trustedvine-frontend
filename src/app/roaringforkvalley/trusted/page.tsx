8ce84a0d0515ced5071b2870711c7822428963c9
Clayton Horning <clayton.horning@colorado.edu>
2024-05-24 10:00:16 -0600

    Changes

--- src/app/roaringforkvalley/trusted/page.tsx
+++ src/app/roaringforkvalley/trusted/page.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { IconZoomExclamation } from "@tabler/icons-react";
 
 export default function Trusted() {
-  return <div>Hello~</div>;
+  return (
+    <div className="flex flex-row items-center justify-center text-gray-400 h-full pb-20">
+      <div className="flex flex-col items-center">
+        <div className="flex flex-row items-center space-x-2">
+          <IconZoomExclamation size={35} />
+          <h2 className="text-2xl">No saved providers</h2>
+        </div>
+        <p>
+          Search for a provider and select the save button
+          to add them here.
+        </p>
+      </div>
+    </div>
+  );
 }
